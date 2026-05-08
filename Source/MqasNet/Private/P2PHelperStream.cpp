dcb645d3e1d35fe7ac5552a837c553e4f98952a7
eqd <1274704958@qq.com>
2026-05-08 11:18:48 +0800

    Refactor P2PHelperStream callbacks to use AsyncTask for thread safety and data integrity

--- Source/MqasNet/Private/P2PHelperStream.cpp
+++ Source/MqasNet/Private/P2PHelperStream.cpp
@@ -35,47 +35,111 @@ P2PHelperStream* P2PHelperStream::GetInstance(GSY_StreamId sid)
 
 void P2PHelperStream::Cb_OnRegistrationSuccess(ErrorCode err, GSY_StreamId sid, GSY_PeerId peerId)
 {
-	if (auto* self = GetInstance(sid)) self->OnRegistrationSuccess(err, sid, peerId);
+	if (auto* const self = GetInstance(sid))
+	{
+		AsyncTask(ENamedThreads::GameThread, [self,err,sid,peerId]()
+		{
+			self->OnRegistrationSuccess(err, sid, peerId);
+		});
+	}
 }
 
 void P2PHelperStream::Cb_OnUnregister(ErrorCode err, GSY_StreamId sid)
 {
-	if (auto* self = GetInstance(sid)) self->OnUnregister(err, sid);
+	if (auto* self = GetInstance(sid))
+	{
+		AsyncTask(ENamedThreads::GameThread, [self, err, sid]()
+		{
+			self->OnUnregister(err, sid);
+		});
+	}
 }
 
 void P2PHelperStream::Cb_OnConnectResponds(ErrorCode err, GSY_PeerId otherPeer, GSY_StreamId sid)
 {
-	if (auto* self = GetInstance(sid)) self->OnConnectResponds(err, otherPeer, sid);
+	if (auto* self = GetInstance(sid))
+	{
+		AsyncTask(ENamedThreads::GameThread, [self, err, otherPeer, sid]()
+		{
+			self->OnConnectResponds(err, otherPeer, sid);
+		});
+	}
 }
 
 void P2PHelperStream::Cb_OnReceivePeerList(GSY_PeerData* list, uint32_t size, GSY_StreamId sid)
 {
-	if (auto* self = GetInstance(sid)) self->OnReceivePeerList(list, size, sid);
+	// 拷贝 peer 列表，避免原始指针在 GameThread 执行前失效
+	TArray<GSY_PeerData> PeerList(list, size);
+	if (auto* self = GetInstance(sid))
+	{
+		AsyncTask(ENamedThreads::GameThread, [self, PeerList = MoveTemp(PeerList), size, sid]() mutable
+		{
+			self->OnReceivePeerList(PeerList.GetData(), size, sid);
+		});
+	}
 }
 
 void P2PHelperStream::Cb_OnPeerReqConnect(GSY_PeerData* peer, GSY_StreamId sid)
 {
-	if (auto* self = GetInstance(sid)) self->OnPeerReqConnect(peer, sid);
+	// 拷贝 peer 数据，避免指针悬空
+	GSY_PeerData PeerCopy = *peer;
+	if (auto* self = GetInstance(sid))
+	{
+		AsyncTask(ENamedThreads::GameThread, [self, PeerCopy, sid]() mutable
+		{
+			self->OnPeerReqConnect(&PeerCopy, sid);
+		});
+	}
 }
 
 void P2PHelperStream::Cb_OnError(GSY_ConnectionHwnd conn, GSY_StreamId sid, ErrorCode err, const char* msg, GSY_RequestId reqId)
 {
-	if (auto* self = GetInstance(sid)) self->OnError(conn, sid, err, msg, reqId);
+	// 拷贝 msg 字符串，避免原始指针在 GameThread 执行前失效
+	FString MsgCopy = msg ? UTF8_TO_TCHAR(msg) : TEXT("");
+	if (auto* self = GetInstance(sid))
+	{
+		AsyncTask(ENamedThreads::GameThread, [self, conn, sid, err, MsgCopy = MoveTemp(MsgCopy), reqId]()
+		{
+			self->OnError(conn, sid, err, TCHAR_TO_UTF8(*MsgCopy), reqId);
+		});
+	}
 }
 
 void P2PHelperStream::Cb_OnChangeToHelperResult(GSY_StreamId sid, ErrorCode err, GSY_PeerId peerId)
 {
-	if (auto* self = GetInstance(sid)) self->OnChangeToHelperResult(sid, err, peerId);
+	if (auto* self = GetInstance(sid))
+	{
+		AsyncTask(ENamedThreads::GameThread, [self, sid, err, peerId]()
+		{
+			self->OnChangeToHelperResult(sid, err, peerId);
+		});
+	}
 }
 
 void P2PHelperStream::Cb_OnAttemptConnect(GSY_StreamId sid, const char* ip, uint16_t port, uint32_t times, uint32_t verifyCode)
 {
-	if (auto* self = GetInstance(sid)) self->OnAttemptConnect(sid, ip, port, times, verifyCode);
+	// 拷贝 ip 字符串，避免原始指针在 GameThread 执行前失效
+	FString IpCopy = ip ? UTF8_TO_TCHAR(ip) : TEXT("");
+	if (auto* self = GetInstance(sid))
+	{
+		AsyncTask(ENamedThreads::GameThread, [self, sid, IpCopy = MoveTemp(IpCopy), port, times, verifyCode]()
+		{
+			self->OnAttemptConnect(sid, TCHAR_TO_UTF8(*IpCopy), port, times, verifyCode);
+		});
+	}
 }
 
 void P2PHelperStream::Cb_OnHelperQuitResult(GSY_StreamId sid, GSY_HelperResult* result)
 {
-	if (auto* self = GetInstance(sid)) self->OnHelperQuitResult(sid, result);
+	// 拷贝 result 数据，避免指针悬空
+	GSY_HelperResult ResultCopy = *result;
+	if (auto* self = GetInstance(sid))
+	{
+		AsyncTask(ENamedThreads::GameThread, [self, sid, ResultCopy]() mutable
+		{
+			self->OnHelperQuitResult(sid, &ResultCopy);
+		});
+	}
 }
 
 void P2PHelperStream::OnRegistrationSuccess(ErrorCode err, GSY_StreamId sid, GSY_PeerId peerId)
